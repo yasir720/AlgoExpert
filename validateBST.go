e8dfde05fb8e5088c72ca12fc5e726db8a2d6c97
Yasir Azizi <yasir720@my.yorku.ca>
2022-02-22 00:08:17 -0500

    updated ValidateBST.go

--- validateBST.go
+++ validateBST.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import "math"
 
-func main() {
-	fmt.Println("hey there")
+type BST struct {
+	Value int
+
+	Left  *BST
+	Right *BST
+}
+
+// O(n) time | O(d) space
+func (tree *BST) ValidateBst() bool {
+	// Write your code here.
+	return tree.validateBst(math.MinInt32, math.MaxInt32) // this set it to the max
+                                                         // possible int32 value.
+}
+
+func (tree *BST) validateBst(minValue, maxValue int) bool {
+    if tree == nil {
+        return true
+    }
+
+    if tree.Value < minValue || tree.Value >= maxValue {
+        return false // meaning that the value of the node is out of the allowed range
+    }
+
+    // we now check the possible child nodes to check validity
+    leftIsValid := tree.Left.validateBst(minValue, tree.Value)
+    rightIsValid := tree.Right.validateBst(tree.Value, maxValue)
+
+    return leftIsValid && rightIsValid
 }
