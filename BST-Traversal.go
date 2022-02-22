97d4f4f4e27fea35f6434108c58283de71c31280
Yasir Azizi <yasir720@my.yorku.ca>
2022-02-22 00:50:25 -0500

    did another one
    good idea to do this one on the whiteboard to really get how node values are being added to the array

---
+++ BST-Traversal.go
@@ -0,0 +1,54 @@
+package main
+
+type BST struct {
+	Value int
+
+	Left  *BST
+	Right *BST
+}
+
+// left node -> current node -> right node.
+// from min to max.
+// O(n) time | O(n) space
+func (tree *BST) InOrderTraverse(array []int) []int {
+	// Write your code here.
+
+	if tree.Left != nil { // go all the way to the left
+		array = tree.Left.InOrderTraverse(array)
+	}
+	array = append(array, tree.Value) // we're only appending here
+	if tree.Right != nil {
+		array = tree.Right.InOrderTraverse(array)
+	}
+	return array
+}
+
+// current node -> left node -> right node.
+// O(n) time | O(n) space
+func (tree *BST) PreOrderTraverse(array []int) []int {
+	// Write your code here.
+
+	array = append(array, tree.Value)
+	if tree.Left != nil {
+		array = tree.Left.PreOrderTraverse(array)
+	}
+	if tree.Right != nil {
+		array = tree.Right.InOrderTraverse(array)
+	}
+	return array
+}
+
+// left node -> right node -> current node.
+// O(n) time | O(n) space
+func (tree *BST) PostOrderTraverse(array []int) []int {
+	// Write your code here.
+	
+	if tree.Left != nil {
+		array = tree.Left.PreOrderTraverse(array)
+	}
+	if tree.Right != nil {
+		array = tree.Right.InOrderTraverse(array)
+	}
+	array = append(array, tree.Value)
+	return array
+}
