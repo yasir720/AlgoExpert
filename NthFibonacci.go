1e93eeb9c77b39854cfdd1aa3bdd40f4312b9139
Yasir Azizi <yasir720@my.yorku.ca>
2022-03-08 00:17:30 -0500

    did a couple more

---
+++ NthFibonacci.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// O(n) time | O(1) space
+func GetNthFib(n int) int {
+	// Write your code here.
+	lastTwo := []int{0, 1}
+
+	if n ==1 {
+		return 0
+	}
+
+	for i := 2; i <= n-1; i++ {
+		nextFib := lastTwo[0] + lastTwo[1]
+		lastTwo[0] = lastTwo[1]
+		lastTwo[1] = nextFib
+	}
+	return lastTwo[1]
+}
+
+func main() {
+	result := GetNthFib(1)
+	fmt.Println(result)
+}
---
+++ ProductSum.go
@@ -0,0 +1,27 @@
+package main
+
+type SpecialArray []interface{}
+
+// Tip: Each element of `array` can either be cast
+// to `SpecialArray` or `int`.
+func ProductSum(array SpecialArray) int {
+	// Write your code here.
+	return helper(array, 1)
+}
+
+func helper(array SpecialArray, multiplier int) int {
+	sum := 0
+
+	for _, element := range array {
+		if cast, ok := element.(SpecialArray); ok {
+			sum = sum + helper(cast, multiplier + 1)
+		} else if cast, ok := element.(int); ok {
+			sum = sum + cast
+		}
+	}
+	return sum * multiplier
+}
+
+func main() {
+
+}
