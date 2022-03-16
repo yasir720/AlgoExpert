e2f558f191c06beb887bd2bd32ed068533bdc90a
Yasir Azizi <yasir720@my.yorku.ca>
2022-03-16 19:51:04 -0400

    did another

---
+++ Graphs/SingleCycleCheck.go
@@ -0,0 +1,26 @@
+package main
+
+func getNextIndex (currentIdx int, array []int) int {
+	hopLength := array[currentIdx]
+	nextIdx := (currentIdx + hopLength) % len(array)
+	
+	if nextIdx >= 0 {
+		return nextIdx
+	}
+	return nextIdx + len(array)
+}
+
+func HasSingleCycle(array []int) bool {
+	// Write your code here.
+	numberOfVisitedElements := 0
+	currentIdx := 0
+
+	for numberOfVisitedElements < len(array) {
+		if numberOfVisitedElements > 0 && currentIdx == 0 {
+			return false
+		}
+		numberOfVisitedElements = numberOfVisitedElements + 1
+		currentIdx = getNextIndex(currentIdx, array)
+	}	
+	return currentIdx == 0
+}
