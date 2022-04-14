db7f7290fb47bf5cd834998aea94a8fbf188af0a
Yasir Azizi <yasir720@my.yorku.ca>
2022-04-13 22:50:38 -0400

    finished off another

--- Searching/FindThreeLargestNumbers.go
+++ Searching/FindThreeLargestNumbers.go
@@ -1,7 +1,35 @@
 package main
 
+import "math"
+
 // O(n) time | O(1)	space
 func FindThreeLargestNumbers(array []int) []int {
 	// Write your code here.
-	return nil
+	threeLargestNumbers := []int{math.MinInt32, math.MinInt32, math.MinInt32}
+
+	for _, number := range array {
+		updateLargestNumbers(threeLargestNumbers, number)
+	}
+
+	return threeLargestNumbers
+}
+
+func updateLargestNumbers(threeLargestNumbers []int, number int) {
+	if number > threeLargestNumbers[2] {
+		sortThruple(threeLargestNumbers, number, 2)
+	} else if number > threeLargestNumbers[1] {
+		sortThruple(threeLargestNumbers, number, 1)
+	} else if number > threeLargestNumbers[0] {
+		sortThruple(threeLargestNumbers, number, 0)
+	}
+}
+
+func sortThruple(threeLargestNumbers []int, number, idx int) {
+	for i := 0; i < idx+1; i++ {
+		if i == idx {
+			threeLargestNumbers[i] = number
+		} else {
+			threeLargestNumbers[i] = threeLargestNumbers[i+1]
+		}
+	}
 }
