4b720bc86c46dba78877817d8ce91e6fef4af7eb
Yasir Azizi <yasir720@my.yorku.ca>
2022-05-06 01:19:45 -0400

    another in the books

---
+++ Dynamic Programming/MaxSubsetSumNoAdjacent.go
@@ -0,0 +1,28 @@
+package main
+
+func MaxSubsetSumNoAdjacent(array []int) int {
+	// Write your code here.
+	if len(array) == 0 {
+		return 0
+	} else if len(array) == 1 {
+		return array[0]
+	}
+
+	first := max(array[0], array[1])
+	second := array[0]
+
+	for i := 2; i < len(array); i++ {
+		current := max(first, second + array[i])
+		second = first
+		first = current
+	}
+	return first
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
